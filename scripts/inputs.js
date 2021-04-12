6928930c3c30c7a79884c9d6000ff0eab8a1a807
Moises Trejo <moisest@mit.edu>
2021-04-12 00:12:55 -0500

    added comparison

--- scripts/inputs.js
+++ scripts/inputs.js
@@ -6,11 +6,11 @@ const generateUserControls = function(initialValues) {
     var userInputHtml = "";
     for (var i=0; i<categories.length; i++) {
         var categoryName = categories[i];
-        userInputHtml += `<div class="categoryInput">`;
+        userInputHtml += `<div class="categoryInput" id="${categoryName}-div">`;
         userInputHtml += `${categoryName} = `;
         userInputHtml += `</label>`;
-        userInputHtml += `<input min="0" max="100" type="number" step="5" id="${categoryName}-input" value="${initialValues[categoryName]}"`;
-        userInputHtml += `onchange="categoryInputChange('${categoryName}');">%</input>`;
+        userInputHtml += `<div class="textinput"><input min="0" max="100" type="number" step="5" id="${categoryName}-input" value="${initialValues[categoryName]}"`;
+        userInputHtml += `onchange="categoryInputChange('${categoryName}');">%</input></div>`;
         userInputHtml += `</div>`;
     }
 
@@ -47,10 +47,47 @@ const btnClick = function() {
     pieInnerRadius = pieRadius * 0.4;
     pieChart.update(getObjValuesAsArray(userInput, categories));
 
+    
+    // d3.select(`#Food-div`).append('p').text('yerr')
+    for (var i = 0; i<categories.length; i++) {
+        d3.select(`#${categories[i]}-div`)
+            .append('p')
+            .text('vs ')
+            .attr('class', 'difference')
+            .append("strong")
+            .text((getSubTotal(i)/getTotal()*100).toFixed(2)+ '%')
+            .attr('class', 'difference')
+    }
+    
+
     // SHOW SUNBURST
     SunBurst(consumptionVizData);
     d3.select("button").attr("disabled", true);
     d3.selectAll("input").attr("disabled", true);
+}
+
+const getSubTotal = (i) => {
+
+    let sum = 0
+    let inner = consumptionVizData['children'][i]
+    for (var j= 0; j < inner['children'].length; j++) {
+        sum += parseFloat(inner['children'][j]['value'])
+    }
+    return sum
+
+}
+
+const getTotal = () => {
+
+    let sum = 0
+    let inner = consumptionVizData['children']
+    for (var i = 0; i < inner.length; i++) {
+        let category = inner[i]
+        for (var j = 0; j < category['children'].length; j++) {
+            sum += parseFloat(category['children'][j]['value'])
+        }
+    }
+    return sum
 }
 
 // INPUT VALIDATOR
